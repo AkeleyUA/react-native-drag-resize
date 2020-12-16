5dc8baeaaa12efac5dafd26ef0add29d60368239
Alexandr Stepaniuk <Akeley1994@gmail.com>
2020-12-16 20:53:27 +0200

    Update DragResizeBlock.js

--- src/DragResizeBlock.js
+++ src/DragResizeBlock.js
@@ -657,6 +657,7 @@ export class DragResizeBlock extends Component {
   renderConnectors = () => {
     const {
       connectors,
+      centerButton
     } = this.props;
 
     const {
@@ -666,16 +667,18 @@ export class DragResizeBlock extends Component {
 
     return connectors.map((connectorType) => {
       if(connectorType === CONNECTOR_CENTER) {
-        <CenterConnector 
-          key={connectorType}
-          type={connectorType}
-          size={CONNECTOR_SIZE}
-          x={this.connectorsMap[CONNECTOR_TOP_LEFT].calculateX(w)}
-          y={this.connectorsMap[CONNECTOR_TOP_LEFT].calculateY(h)}
-          onStart={this.connectorsMap[connectorType].onStart}
-          onMove={this.connectorsMap[connectorType].onMove}
-          onEnd={this.connectorsMap[connectorType].onEnd}
-        >{centerButton}</CenterConnector>
+        return (
+          <CenterConnector 
+            key={connectorType}
+            type={connectorType}
+            size={CONNECTOR_SIZE}
+            x={this.connectorsMap[CONNECTOR_TOP_LEFT].calculateX(w)}
+            y={this.connectorsMap[CONNECTOR_TOP_LEFT].calculateY(h)}
+            onStart={this.connectorsMap[connectorType].onStart}
+            onMove={this.connectorsMap[connectorType].onMove}
+            onEnd={this.connectorsMap[connectorType].onEnd}
+          >{centerButton()}</CenterConnector>
+        )
       }
       return (
         <Connector
